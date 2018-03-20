b36d5b85100285542fdabcf89dc0d33b1b8dd491
jeremy richard <jeremy.richard@sciencespo.fr>
2018-03-20 15:45:49 +0100

    add mailer

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM python:2.7-alpine
 COPY requirements.txt /app/requirements.txt
 
 
-RUN apk --update add gcc musl-dev curl \
+RUN apk --update add gcc musl-dev curl ssmtp mailx \
         &&  pip install --no-cache-dir --requirement /app/requirements.txt \
         && curl -sL https://github.com/michaloo/go-cron/releases/download/v0.0.2/go-cron.tar.gz | tar -x -C /usr/local/bin \
         && apk del gcc musl-dev curl \    
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-exec go-cron "${CRON_SCHEDULE}" /bin/sh -c "python /clean_old_corpora.py ${DAYSBACK}"
+if [ ! -z "$MAILER_SMTP" ] && [ ! -z "$MAILER_PORT" ] && [ ! -z "$MAILER_FROM" ] && [ ! -z "$MAILER_TO" ]; then
+	echo "root:${MAILER_FROM}:${MAILER_SMTP}:${MAILER_PORT}" > /etc/ssmtp/revaliases
+	exec go-cron "${CRON_SCHEDULE}" /bin/sh -c "/usr/local/bin/python /clean_old_corpora.py ${DAYSBACK} | mail -s 'cron ${HOSTNAME}' ${MAILER_TO}"
+else
+	echo "Mailer variables not defined, logging to stdout..."
+	exec go-cron "${CRON_SCHEDULE}" /bin/sh -c "python /clean_old_corpora.py ${DAYSBACK}"
+fi
