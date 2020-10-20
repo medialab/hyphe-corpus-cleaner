1639abe697ccccc3e5adefeb17d5330dccf29f95
Joris Jourdain <70632176+pipojojo@users.noreply.github.com>
2020-10-20 16:25:39 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 SHELL="/bin/sh -c"
 CMD="/usr/local/bin/python /clean_old_corpora.py ${DAYSBACK:-7}"
-MAIL_TIMEOUT=${MAIL_TIMEOUT:-2}
+MAIL_TIMEOUT=${MAIL_TIMEOUT:-10}
 MAIL_AND_CONSOLE=${MAIL_AND_CONSOLE:-1}
                                                                                                                      
 
@@ -11,11 +11,11 @@ if [ ! -z "$MAILER_HOST" ] && [ ! -z "$MAILER_PORT" ] && [ ! -z "$MAILER_FROM" ]
         MAIL_COMMAND="| mail -s \"Mail from hyphe cleaner on ${HOSTNAME}\" ${MAILER_TO}"
 	if [ -z "$CRON_SCHEDULE" ] ; then MAIL_COMMAND="$MAIL_COMMAND && sleep $MAIL_TIMEOUT" ; fi
 	if [ "$MAIL_AND_CONSOLE" != "0" ] ; then
-	        echo "Also logging to stdout as since "MAIL_AND_CONSOLE" is not set to "0""
+	        echo "Also logging to console since "MAIL_AND_CONSOLE" is not set to "0""
 	        TEE="| tee /dev/stderr"
 	fi
 else
-        echo "Mailer variables not defined, logging to stdout only..."
+        echo "Mailer variables not defined, logging to console only..."
 fi
 
 if [ ! -z "$CRON_SCHEDULE" ] ; then GOCRON="go-cron "${CRON_SCHEDULE}" " ; fi
